9f5794225fb389f24edfeee6a88678a86e167a52
Robyn Klopp <rklopp@gmail.com>
2019-12-09 13:00:22 -0800

    Define list of santas, randomly select a person from the list to be the giftee of the first santa

--- secret_santa.py
+++ secret_santa.py
@@ -2,8 +2,22 @@
 
 from random import randint
 
-name_list = ["Hilary", "Rich", "Roz", "Derek", "Robyn", "Cory", "Monica", "Peter", "Vicky", "Kathryn"]
-list_length = len(name_list)
-match_index = randint(0, list_length)
+##Create a list of all santas
+santa_list = ["Hilary", "Rich", "Roz", "Derek", "Robyn", "Cory", "Monica", "Peter", "Vicky", "Kathryn"]
+gifter = santa_list[0]
 
-print(name_list)
+##Remove our gifter from the list of recipients
+santa_list.remove(gifter)
+recipient_list = santa_list
+print(recipient_list)
+
+##Select a random recipient
+recipient_index = randint(0, (len(recipient_list)-1))
+recipient_name = recipient_list[recipient_index]
+##Display gifter and recipient
+print(gifter + " is giving to: " + recipient_name)
+
+recipient_list.pop(recipient_index)
+print(recipient_list)
+
+##print(recipient_list)
