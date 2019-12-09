f354196d53501dcdd101b5ee8a39326476d07d63
Robyn Klopp <rklopp@gmail.com>
2019-12-09 14:00:57 -0800

    Turn it into a function that iterates until all the list items are gone.

--- secret_santa.py
+++ secret_santa.py
@@ -2,22 +2,31 @@
 
 from random import randint
 
-##Create a list of all santas
+##Create a list of all santas and all giftees
+
 santa_list = ["Hilary", "Rich", "Roz", "Derek", "Robyn", "Cory", "Monica", "Peter", "Vicky", "Kathryn"]
-gifter = santa_list[0]
+giftee_list = santa_list.copy()
 
-##Remove our gifter from the list of recipients
-santa_list.remove(gifter)
-recipient_list = santa_list
-print(recipient_list)
+def select_recipient():
 
-##Select a random recipient
-recipient_index = randint(0, (len(recipient_list)-1))
-recipient_name = recipient_list[recipient_index]
-##Display gifter and recipient
-print(gifter + " is giving to: " + recipient_name)
+    ##Select the next santa and remove our gifter from the master list of santas
+    gifter = santa_list[0]
+    santa_list.remove(gifter)
 
-recipient_list.pop(recipient_index)
-print(recipient_list)
+    ##make a working list of recipients and remove our gifter if not already gone
+    recipient_list = giftee_list.copy()
+    if gifter in recipient_list: recipient_list.remove(gifter)
+    ##Select a random recipient
+    recipient_index = randint(0, (len(recipient_list)-1))
+    recipient_name = recipient_list[recipient_index]
 
-##print(recipient_list)
+    ##Display gifter and recipient
+    print(gifter + " is giving to: " + recipient_name)
+
+    ##Remove giftee from master list of recipients
+    giftee_list.remove(recipient_name)
+    print(santa_list)
+
+
+for i in range(len(santa_list)):
+    select_recipient()
