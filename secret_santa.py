6b2720243bc23e17cf40e84640c6e89dbe0c59d3
Robyn Klopp <rklopp@gmail.com>
2019-12-09 17:48:37 -0800

    add some user input, todo check input

--- secret_santa.py
+++ secret_santa.py
@@ -1,11 +1,19 @@
 # "Secret Santa" 2.0 Takes a list of names and assigns each to another person.
 
-from random import shuffle
+import random
 
 # Create a list of all santas
-santa_list = ["one", "two", "three", "four", "five"]
 
-shuffle(santa_list)
+print("Please enter 3 or more names separated by a space, then press enter")
+
+
+santa_list = input().split(" ")
+
+if len(santa_list) < 3: print("Whoops! Please try three or more names separated by a space.")
+
+random.shuffle(santa_list)
 for i in range(len(santa_list) - 1):
     print(santa_list[i] + " is giving to " + santa_list[i+1])
 print(santa_list[i+1] + " is giving to " + santa_list[0])
+
+
