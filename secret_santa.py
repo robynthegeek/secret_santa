e9febb2a259107eba051622b43a883e077fc9ae6
Robyn Klopp <rklopp@gmail.com>
2019-12-09 18:25:04 -0800

    Update to scrub spaces or blank entries from user input

--- secret_santa.py
+++ secret_santa.py
@@ -1,12 +1,10 @@
 # "Secret Santa" 2.0 Takes a list of names and assigns each to another person.
 
 import random
-
-# Create a list of all santas
 from typing import List
-print("Please enter 3 or more names separated by a space, then press enter")
 
 
+# Sorts santas randomly and matches each with a recipient
 def shuffle_santas(santa_list):
     random.shuffle(santa_list)
     for i in range(len(santa_list) - 1):
@@ -14,13 +12,20 @@ def shuffle_santas(santa_list):
     print(santa_list[i+1] + " is giving to " + santa_list[0])
 
 
+# Accepts user input list and checks for minimum length, passes list to shuffle_santas
 def add_santas():
     santa_input_list: List[str] = input().split(" ")
-    if len(santa_input_list) < 3:
+    clean_input_list = list(filter(None, santa_input_list))
+    if len(clean_input_list) < 3:
         print("Whoops! Please try three or more names separated by a space.")
         add_santas()
     else:
-        shuffle_santas(santa_input_list)
+        shuffle_santas(clean_input_list)
+
+
+# Request user input of list of names
+print("Secret Santa Sorter 2.0; Sorting Santas Since 2019")
+print("Please enter 3 or more names separated by spaces, then press enter")
 
 add_santas()
 
