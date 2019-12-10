a1c8b622ee63f3cf5f99bebe7e3c290c8cbf7e15
Robyn Klopp <rklopp@gmail.com>
2019-12-09 17:58:06 -0800

    add Validation of user input for number of inputs

--- secret_santa.py
+++ secret_santa.py
@@ -3,17 +3,26 @@
 import random
 
 # Create a list of all santas
-
+from typing import List
 print("Please enter 3 or more names separated by a space, then press enter")
 
 
-santa_list = input().split(" ")
+def shuffle_santas(santa_list):
+    random.shuffle(santa_list)
+    for i in range(len(santa_list) - 1):
+        print(santa_list[i] + " is giving to " + santa_list[i+1])
+    print(santa_list[i+1] + " is giving to " + santa_list[0])
 
-if len(santa_list) < 3: print("Whoops! Please try three or more names separated by a space.")
 
-random.shuffle(santa_list)
-for i in range(len(santa_list) - 1):
-    print(santa_list[i] + " is giving to " + santa_list[i+1])
-print(santa_list[i+1] + " is giving to " + santa_list[0])
+def add_santas():
+    santa_input_list: List[str] = input().split(" ")
+    if len(santa_input_list) < 3:
+        print("Whoops! Please try three or more names separated by a space.")
+        add_santas()
+    else:
+        shuffle_santas(santa_input_list)
+
+add_santas()
+
 
 
